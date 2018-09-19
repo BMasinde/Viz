24aac1b3e012fb510919ed74ae77d20c6ef573bc
BMasinde <masindeb@live.com>
2018-09-19 11:13:29 +0200

    myplot fxn

--- olive2.R
+++ olive2.R
@@ -3,3 +3,11 @@
 
 baseball <- read.csv("baseball-2016.csv", sep = ",", header = TRUE)
 
+myplot <-function(df, y_string){
+  ggplot(df, aes_string(x = "V2", y = y_string, color = "League"))+
+    geom_point()+
+    geom_vline(xintercept = 0, color = "black")+
+    geom_hline(yintercept = 0, color = "black")
+}
+
+myplot(d4, "Runs.per.game")
